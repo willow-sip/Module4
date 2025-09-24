9003aba452928fb090f41128140604583d7466d5
Diana <102168174+willow-sip@users.noreply.github.com>
2025-09-24 19:00:24 +0300

    fix: made some changes in task 4

--- TypeScript-exercises-practice/task4.ts
+++ TypeScript-exercises-practice/task4.ts
@@ -22,11 +22,11 @@ export const persons: Person[] = [
 ];
 
 export function isAdmin(person: Person): person is Admin{
-    return (person as Admin).type === 'admin';
+    return person.type === 'admin';
 }
 
 export function isUser(person: Person): person is User {
-    return (person as User).type === 'user';
+    return person.type === 'user';
 }
 
 export function logPerson(person: Person) {
